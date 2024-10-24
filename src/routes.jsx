4cff11ff7ef7ee74894b774071b03eda8967baf8
Bleri123 <bleonnasufi9@gmail.com>
2024-10-24 21:48:07 +0200

    fixed some '' to ""

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./pages/App";
 import LoginPage from "./pages/Login";
-import Error from './pages/Error'
+import Error from "./pages/Error";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App/>,
-        errorElement: <Error/>
-    },
-    {
-        path: '/login',
-        element: <LoginPage/>
-    }
-])
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+]);
 
 export default router;
