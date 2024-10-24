4c0e3c75dccdb3b8237d946432e20ab2d1f348ab
Elton Rexha <elton.rexha123@outlook.com>
2024-10-24 19:57:08 +0200

    Add propTypes, make a request to the server on login
    Add propTypes, made a request to the server to login
    and store the token on the localStorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,17 @@
+import PropTypes from 'prop-types';
 import BOKLogo from '../assets/BOKLOGO.png';
 import { Formik, Form, Field } from 'formik';
+import axios from 'axios';
 import * as Yup from 'yup';
+import { useState } from 'react';
 
 const SignupSchema = Yup.object().shape({
   email: Yup.string().email('Invalid email').required('Email is required'),
   pin: Yup.string().max(32).required('Pin is required'),
 });
 
-//Bonja ignore proptypes se i spjegojna djal
 function CustomField({ name, type, placeholder, isValid }) {
-  console.log({isValid})
+  console.log({ isValid });
   return (
     <Field
       name={name}
@@ -26,67 +28,93 @@ function CustomError({ children }) {
   return <div className="text-red-600">{children}</div>;
 }
 
-export const LoginPage = () => (
-  <div className="bg-slate-800 font-poppins min-h-[100vh] flex flex-col items-center ">
-    <div className="flex flex-col items-center max-w-[700px] w-full md:w-[80%]">
-      <img src={BOKLogo} alt="bank of kosovo logo" className="w-56 h-auto" />
-      <div className="bg-gray-300 sm:w-[60%] w-full rounded-sm">
-        <h1 className="text-center p-5 text-5xl text-slate-700 mb-2 border-b-2 border-slate-800">
-          Login
-        </h1>
-        <Formik
-          initialValues={{
-            email: '',
-            pin: '',
-          }}
-          validationSchema={SignupSchema}
-          onSubmit={(values) => {
-            // same shape as initial values
-            console.log(values);
-          }}
-        >
-          {({ errors, touched }) => (
-            <Form className="flex flex-col items-center gap-2 ml-10 mr-10 ps-2 pb-5 pt-5">
-              <CustomField
-                name="email"
-                type="text"
-                placeholder="Email"
-                isValid={errors?.email === null}
-              />
+export function LoginPage() {
+  const [error, setError] = useState(null);
+  return (
+    <div className="bg-slate-800 font-poppins min-h-[100vh] flex flex-col items-center ">
+      <div className="flex flex-col items-center max-w-[700px] w-full md:w-[80%]">
+        <img src={BOKLogo} alt="bank of kosovo logo" className="w-56 h-auto" />
+        <div className="bg-gray-300 sm:w-[60%] w-full rounded-sm">
+          <h1 className="text-center p-5 text-5xl text-slate-700 mb-2 border-b-2 border-slate-800">
+            Login
+          </h1>
+          <Formik
+            initialValues={{
+              email: '',
+              pin: '',
+            }}
+            validationSchema={SignupSchema}
+            onSubmit={async (values) => {
+              // same shape as initial values
+              console.log(values);
+              try {
+                const res = await axios.post(
+                  'http://localhost:5000/auth/login',
+                  values
+                );
+                setError(null);
+                localStorage.setItem('token', res.data.token);
+              } catch (e) {
+                setError(e.response.data.error);
+              }
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form className="flex flex-col items-center gap-2 ml-10 mr-10 ps-2 pb-5 pt-5">
+                <CustomField
+                  name="email"
+                  type="text"
+                  placeholder="Email"
+                  isValid={!errors.email}
+                />
 
-              {errors.email && touched.email ? (
-                <CustomError>{errors.email}</CustomError>
-              ) : null}
+                {errors.email && touched.email ? (
+                  <CustomError>{errors.email}</CustomError>
+                ) : null}
 
-              <CustomField
-                name="pin"
-                type="password"
-                placeholder="Password"
-                isValid={errors?.email === null}
-              />
+                <CustomField
+                  name="pin"
+                  type="password"
+                  placeholder="Password"
+                  isValid={!errors.pin}
+                />
 
-              {errors.pin && touched.pin && (
-                <CustomError>{errors.pin}</CustomError>
-              )}
+                {errors.pin && touched.pin && (
+                  <CustomError>{errors.pin}</CustomError>
+                )}
 
-              <button
-                type="submit"
-                className="border-2 w-full rounded-full hover:bg-gray-800 transition-colors ease-in-out delay-50 hover:text-white duration-100  border-gray-800 mt-5 p-2"
-              >
-                Submit
-              </button>
-              <div className="text-sm text-pretty text-center">
-                <p>Sign Up Information:</p> At the moment, signing up for an
-                account cannot be completed online. To create a new account,
-                please visit us in person at our physical location. We apologize
-                for any inconvenience and appreciate your understanding.
-              </div>
-            </Form>
-          )}
-        </Formik>
+                <button
+                  type="submit"
+                  className="border-2 w-full rounded-full hover:bg-gray-800 transition-colors ease-in-out delay-50 hover:text-white duration-100  border-gray-800 mt-5 p-2"
+                >
+                  Submit
+                </button>
+                {error && <CustomError>{error}</CustomError>}
+                <div className="text-sm text-pretty text-center">
+                  <p>Sign Up Information:</p> At the moment, signing up for an
+                  account cannot be completed online. To create a new account,
+                  please visit us in person at our physical location. We
+                  apologize for any inconvenience and appreciate your
+                  understanding.
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
+
+CustomError.propTypes = {
+  children: PropTypes.any,
+};
+
+CustomField.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  placeholder: PropTypes.string,
+  isValid: PropTypes.bool,
+};
 
 export default LoginPage;
