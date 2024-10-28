34b91c5bb802d73596af8c03522ecf0b01815dba
Elton Rexha <elton.rexha123@outlook.com>
2024-10-28 20:47:31 +0100

    change size of header

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -5,8 +5,8 @@ export default function DashboardHeader({
   accountNumber
 }) {
   return (
-    <header className="bg-primary h-52 flex">
-      <div className="w-64 h-full border-r-2 border-b-2 border-stroke">
+    <header className="bg-primary h-28 flex">
+      <div className="w-[10%] h-full border-r-2 border-b-2 border-stroke">
         <img
           className="w-[100%] h-full"
           src={BOKLogo}
@@ -15,15 +15,15 @@ export default function DashboardHeader({
       </div>
       <div className="p-2 text-secondary flex-1">
         <div className="flex h-[50%] items-center pr-20 pl-4 pt-2 pb-4 border-b-2 ">
-          <h1 className="mr-[auto] text-6xl font-bold w-min">BOK</h1>
+          <h1 className="mr-[auto] text-4xl font-bold w-min">BOK</h1>
           {accountNumber && (
-            <p className="text-2xl">
+            <p className="text-xl">
               <span>Selected account number: </span> {accountNumber}
             </p>
           )}
         </div>
         <div className="h-[50%] flex items-center justify-center">
-          <h1 className="text-4xl font-medium">Bank Of Kosovo</h1>
+          <h1 className="text-2xl font-medium">Bank Of Kosovo</h1>
         </div>
       </div>
     </header>
