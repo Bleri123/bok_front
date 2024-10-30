593ee539115755be7a8a54cbefa671772b168ba1
Bleri123 <bleonnasufi9@gmail.com>
2024-10-30 15:28:55 +0100

    finished homepage mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,21 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div id="header" className="bg-primary text-white flex items-center">
+    <div
+      id="header"
+      className="bg-primary text-white flex items-center sm:h-32 lg:h-[152px] xl:h-[180px]"
+    >
       <div id="logo" className="w-[40%] border-r flex justify-center">
         <Link to="/">
           <img
             src="/images/BOK.png"
             alt="BOK"
-            className="w-[125px] h-[85px] object-cover"
+            className="w-[175px] h-[110px] object-cover lg:w-[200px] lg:h-[145px] xl:w-[300px] xl:h-[220px]"
           />
         </Link>
       </div>
       <div id="nav-bar" className="w-[60%]">
-        <ul className="flex gap-3 justify-center text-sm font-bold">
+        <ul className="flex gap-3 justify-center text-sm font-bold md:text-xl lg:gap-5 lg:text-2xl xl:gap-7 xl:text-3xl">
           <li className="link-style">
             <Link to="/login">Login</Link>
           </li>
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,20 +2,30 @@ import { Header } from "../components/Header";
 
 function App() {
   return (
-    <div className="bg-secondary min-h-[100vh]">
+    <div className="bg-secondary min-h-screen flex flex-col">
       <Header />
-      <div id="content" className="flex justify-center mt-12  bg-secondary">
-        <div id="left" className="w-[100%] ml-8">
+      <div
+        id="content"
+        className="flex flex-col lg:flex-row justify-center mt-12 bg-secondary px-4 flex-grow"
+      >
+        <div
+          id="left"
+          className="ml-3 w-full lg:w-[50%] flex flex-col xl:w-[60%] 2xl:ml-8"
+        >
           <div id="Welcome">
-            <h1 className="font-bold text-[28px]">Welcome to Bank of Kosovo</h1>
-            <p className="text-[15px] w-[65%] text-pretty pl-4">
+            <h1 className="font-bold text-[28px] md:text-5xl xl:text-6xl">
+              Welcome to Bank of Kosovo
+            </h1>
+            <p className="mt-4 text-[15px] w-full text-pretty pl-4 md:text-2xl md:w-[550px] xl:text-3xl xl:w-[650px]">
               Welcome to the Bank Of Kosovo, a professional bank system, made
               for online transactions like a boss.
             </p>
           </div>
           <div id="list" className="mt-8">
-            <h1 className="font-bold text-[28px]">Use online banking</h1>
-            <ul className="list-disc ml-4 text-[15px] w-[65%] pl-4">
+            <h1 className="font-bold text-[28px] md:text-5xl xl:text-6xl 2xl:mt-8">
+              Use online banking
+            </h1>
+            <ul className="mt-4 list-disc text-[15px] w-full pl-4 md:text-2xl xl:text-3xl">
               <li>Receive money</li>
               <li>Send money</li>
               <li>See your balance</li>
@@ -24,17 +34,23 @@ function App() {
             </ul>
           </div>
         </div>
-      </div>
-      <div id="right" className="w-[100%] flex justify-center">
-        <div id="card_photo">
-          <img
-            src="/images/card.png"
-            alt="card"
-            className="w-[250px] h-[150px]"
-          />
+        <div
+          id="right"
+          className="w-full lg:w-[50%] flex justify-center items-center xl:w-[40%]"
+        >
+          <div id="card_photo">
+            <img
+              src="/images/card.png"
+              alt="card"
+              className="w-[250px] h-[150px] md:w-[340px] md:h-[240px] lg:w-[420px] lg:h-[330px] 2xl:w-[620px] 2xl:h-[430px]"
+            />
+          </div>
         </div>
       </div>
-      <div id="footer" className="text-center text[primary] mt-8">
+      <div
+        id="footer"
+        className="flex justify-center text[primary] mt-8 align-end"
+      >
         <p>@COPYRIGHT 2024 BOK</p>
       </div>
     </div>
