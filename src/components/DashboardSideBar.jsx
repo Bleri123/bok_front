9a4b273e0091f3cd6fdcf997573fe6fb8acdd358
Bler123 <bleonnasufi9@gmail.com>
2024-11-01 13:05:38 +0100

    Did some changes to font and spacings

--- src/components/DashboardSideBar.jsx
+++ src/components/DashboardSideBar.jsx
@@ -4,11 +4,10 @@ import { Link } from "react-router-dom";
 export default function DashboardSideBar({ show, links }) {
   return (
     <div
-      className={`w-[200px] bg-primary lg:w-[200px] xl:w-[240px] flex flex-col h-[400px] md:h-[400px] lg:h-[400px] xl:h-[500px] ${
-        show ? "block" : "hidden"
-      } lg:block`}
+      className={`w-[200px] bg-primary lg:w-[200px] xl:w-[240px] flex flex-col style="
+    height: 100vh; ${show ? "block" : "hidden"} lg:block`}
     >
-      <ul className="text-tprimary flex items-center flex-col text-[25px] gap-4 md:w-[200px] lg:w-[200px] lg:text-[35px] xl:w-[240px]">
+      <ul className="text-tprimary flex items-center flex-col text-[25px] gap-4 md:w-[200px] lg:w-[200px] lg:text-[30px] xl:w-[240px]">
         {links.map(({ link, text }) => (
           <li key={text}>
             <Link to={link}>{text}</Link>
