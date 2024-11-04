40a4115444c46a0ea1a0f131d96bf6bb5e0df6f3
Bler123 <bleonnasufi9@gmail.com>
2024-11-04 21:34:02 +0100

    Deposit Finished 90%

--- src/pages/dashboardPages/Deposit.jsx
+++ src/pages/dashboardPages/Deposit.jsx
@@ -1,3 +1,35 @@
+import { Footer } from "../../components/Footer";
+
 export default function Deposit() {
-  return <h1>This is Deposit</h1>;
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <div className="flex-grow flex flex-col items-center mt-11">
+        <h1 className="mb-1 text-tprimary text-3xl lg:text-5xl">Deposit</h1>
+        <div
+          id="Cards"
+          className="flex flex-col bg-secondary items-center p-4 w-[200px] rounded-md mt-6 md:w-[450px] lg:w-[670px]"
+        >
+          <div id="card" className="flex flex-col items-center mb-4">
+            <button className="bg-primary text-white p-2 rounded w-[150px] h-[70px] text-2xl md:w-[350px] md:h-[100px] md:text-3xl lg:w-[450px] lg:text-4xl mt-5">
+              Credit
+            </button>
+            <p className="text-lg md:text-2xl lg:text-3xl">400,00€</p>
+          </div>
+          <div id="card" className="flex flex-col items-center mb-4">
+            <button className="bg-primary text-white p-2 rounded w-[150px] h-[70px] text-2xl md:w-[350px] md:h-[100px] md:text-3xl lg:w-[450px] lg:text-4xl">
+              Debit
+            </button>
+            <p className="text-lg md:text-2xl lg:text-3xl">20,00€</p>
+          </div>
+          <div id="card" className="flex flex-col items-center mb-4">
+            <button className="bg-primary text-white p-2 rounded w-[150px] h-[70px] text-2xl md:w-[350px] md:h-[100px] md:text-3xl lg:w-[450px] lg:text-4xl">
+              Savings
+            </button>
+            <p className="text-lg md:text-2xl lg:text-3xl">1600,00€</p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
