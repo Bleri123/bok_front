5bd379e4ba1db2247791e76f1c69638a11deb66e
AlmedinNasufi <almedin.nasufi20@gmail.com>
2025-01-22 19:10:37 +0100

    fixed the logo stuff

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import BOKLogo from "../assets/BOKLOGO.png";
+import newBOKlogo from "../assets/newBOKlogo.png";
 import getSelectedAccount from "../utils/getSelectedAccount";
 import { useState } from "react";
 
